3998a466831793b640ca9872fc22431ebe1d265f
Adriano Vega Llobell <adrianovega@hotmail.com>
2020-08-10 19:09:39 +0200

    Created the Program.cs file that reads from the thermometer

---
+++ RaspieThermometer.Cli/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Starkie.RaspieThermometer.Cli
+{
+    using System;
+
+    class Program
+    {
+        private const string DevicesDirectory = "/sys/bus/w1/devices/";
+
+        static void Main(string[] args)
+        {
+            Thermometer thermometer = new Thermometer(DevicesDirectory);
+            Console.WriteLine(thermometer.Temperature);
+        }
+    }
+}
