d7e425192b9b6e7fd395bd2ef881b16d531f878e
Adriano Vega Llobell <adrianovega@hotmail.com>
2020-08-15 16:12:42 +0200

    Implemented a test for the invalid temperature case

--- Thermometers/Tests/DS18B20ThermometerTests.cs
+++ Thermometers/Tests/DS18B20ThermometerTests.cs
@@ -19,7 +19,7 @@ namespace Thermometers.Tests
         ///     return the current temperature.
         /// </summary>
         [Fact]
-        public void DS18B20Thermometer_Temperature_ShouldReturnExistingTemperature()
+        public void Temperature_SensorReadsTemperatureCorrectly_ShouldReturnExistingTemperature()
         {
             // Arrange.
             IEnumerable<IThermometer> thermometers = DS18B20ThermometerLoader.GetThermometers(DevicesDirectory);
@@ -33,6 +33,32 @@ namespace Thermometers.Tests
 
             // Assert.
             TemperatureMeasurement expectedMeasurement = new TemperatureMeasurement("28-3c01a816c730", MeasurementStatus.Ok, DateTime.Now, 30.062);
+
+            temperatureMeasurement.SensorId.Should().Be(expectedMeasurement.SensorId);
+            temperatureMeasurement.Status.Should().Be(expectedMeasurement.Status);
+            temperatureMeasurement.Temperature.Should().Be(expectedMeasurement.Temperature);
+        }
+
+        /// <summary>
+        ///     The Temperature property. When queried on a properly registered sensor that writes
+        ///     temperatures on an unexpected format, should return an empty measurement with
+        ///     the <see cref="MeasurementStatus.Failed"/> status.
+        /// </summary>
+        [Fact]
+        public void Temperature_CannotParseTemperature_ShouldReturnExistingTemperature()
+        {
+            // Arrange.
+            IEnumerable<IThermometer> thermometers = DS18B20ThermometerLoader.GetThermometers(DevicesDirectory);
+
+            thermometers.Should().HaveCount(1);
+
+            // Act.
+            IThermometer thermometer = thermometers.First();
+
+            TemperatureMeasurement temperatureMeasurement = thermometer.Temperature;
+
+            // Assert.
+            TemperatureMeasurement expectedMeasurement = new TemperatureMeasurement("28-3c01a816c731", MeasurementStatus.Failed, DateTime.Now, default);
 
             temperatureMeasurement.SensorId.Should().Be(expectedMeasurement.SensorId);
             temperatureMeasurement.Status.Should().Be(expectedMeasurement.Status);
