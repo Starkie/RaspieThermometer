8f830a94b7093c6d0dfd96cff9de8ea6dbaeffc2
Adriano Vega Llobell <adrianovega@hotmail.com>
2020-08-15 15:35:04 +0200

    Created the first thermometer test

---
+++ Thermometers/Tests/DS18B20ThermometerTests.cs
@@ -0,0 +1,41 @@
+namespace Thermometers.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using FluentAssertions;
+    using Starkie.RaspieThermometer.Thermometers;
+    using Starkie.RaspieThermometer.Thermometers.Contracts;
+    using Xunit;
+
+    /// <summary> Tests for the DS18B20 Thermometer sensor. </summary>
+    public class DS18B20ThermometerTests
+    {
+        private static readonly string DevicesDirectory = Path.GetFullPath("../../../Resources");
+
+        /// <summary>
+        ///     The Temperature property. When queried on a properly registered sensor, it should
+        ///     return the current temperature.
+        /// </summary>
+        [Fact]
+        public void DS18B20Thermometer_Temperature_ShouldReturnExistingTemperature()
+        {
+            // Arrange.
+            IEnumerable<IThermometer> thermometers = DS18B20ThermometerLoader.GetThermometers(DevicesDirectory);
+
+            thermometers.Should().HaveCount(1);
+
+            // Act.
+            IThermometer thermometer = thermometers.First();
+
+            TemperatureMeasurement temperatureMeasurement = thermometer.Temperature;
+
+            // Assert.
+            TemperatureMeasurement expectedMeasurement = new TemperatureMeasurement("28-3c01a816c730", DateTime.Now, 30.062);
+
+            temperatureMeasurement.SensorId.Should().Be(expectedMeasurement.SensorId);
+            temperatureMeasurement.Temperature.Should().Be(expectedMeasurement.Temperature);
+        }
+    }
+}
